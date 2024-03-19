4363ead9469cd441b03876f6928456214d4a213e
NDHunq <22520514@gm.uit.edu.vn>
2024-03-19 21:03:48 +0700

    UI Add Budget

--- app/src/main/java/com/example/qlct/AddBudget.java
+++ app/src/main/java/com/example/qlct/AddBudget.java
@@ -1,9 +1,7 @@
 package com.example.qlct;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +9,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    TextView hello;
+public class AddBudget extends AppCompatActivity {
+    ImageView date_picker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_budget);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        hello=this.findViewById(R.id.Hello);
-        hello.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, AddBudget.class);
-                startActivity(intent);
-            }
-        });
+        date_picker=this.findViewById(R.id.date_pickerr);
+
     }
 }
