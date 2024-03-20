ce48778d6828673fbcd0c3994519a0f239e24f04
NDHunq <22520514@gm.uit.edu.vn>
2024-03-20 22:27:35 +0700

    xong dialog add budget

--- app/src/main/java/com/example/qlct/Budget_fragment.java
+++ app/src/main/java/com/example/qlct/Budget_fragment.java
@@ -1,5 +1,7 @@
 package com.example.qlct;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,36 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Budget_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import android.widget.TextView;
 public class Budget_fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private TextView create_bud_butt;
 
     public Budget_fragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Budget_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Budget_fragment newInstance(String param1, String param2) {
         Budget_fragment fragment = new Budget_fragment();
         Bundle args = new Bundle();
@@ -45,7 +28,6 @@ public class Budget_fragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,11 +36,19 @@ public class Budget_fragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_budget_fragment, container, false);
+        View view=inflater.inflate(R.layout.fragment_budget_fragment, container, false);
+        create_bud_butt=view.findViewById(R.id.create_bud_butt);
+        create_bud_butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().startActivity(new Intent(getActivity(),AddBudget.class));
+            }
+
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/qlct/MainActivity.java
+++ app/src/main/java/com/example/qlct/MainActivity.java
@@ -16,9 +16,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-
 import com.example.qlct.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
     private void updateButtonBackgrounds(int selectedButtonId) {
@@ -108,5 +107,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
