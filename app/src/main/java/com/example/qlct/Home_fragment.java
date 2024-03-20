da460e11d13d4f6952506c11debfb4d29c0b3a5b
redRUM <22521225@gm.uit.edu.vn>
2024-03-20 11:54:17 +0700

    merge

--- app/src/main/java/com/example/qlct/Home_fragment.java
+++ app/src/main/java/com/example/qlct/Home_fragment.java
@@ -1,5 +1,6 @@
 package com.example.qlct;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Home_fragment extends Fragment {
 
@@ -14,6 +17,32 @@ public class Home_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_fragment, container, false);
+
+        // Tìm ImageView unseen
+        ImageView unseen = view.findViewById(R.id.unseen);
+        TextView text = view.findViewById(R.id.total_blance);
+
+        // Đặt OnClickListener cho unseen
+        unseen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(text.getText().equals("**********"))
+                {
+                    unseen.setBackgroundResource(R.drawable.eye);
+                    text.setText("1000000 đ");
+                }
+
+               else
+                {
+                    text.setText("**********");
+                    unseen.setBackgroundResource(R.drawable.eyedown);
+                }
+
+
+            }
+        });
+
+        return view;
     }
 }
