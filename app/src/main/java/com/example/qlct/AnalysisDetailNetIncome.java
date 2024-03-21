61836cb5e548713e736d9311d8e06926606255b5
NDHunq <22520514@gm.uit.edu.vn>
2024-03-21 23:28:47 +0700

    Done Detail Net Income Activity

--- app/src/main/java/com/example/qlct/AnalysisDetailNetIncome.java
+++ app/src/main/java/com/example/qlct/AnalysisDetailNetIncome.java
@@ -1,6 +1,9 @@
 package com.example.qlct;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnalysisDetailNetIncome extends AppCompatActivity {
+    ListView listView;
+    List<AnalysisNetIcome> arraylist;
+    ImageView exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,29 @@ public class AnalysisDetailNetIncome extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listView=this.findViewById(R.id.listvieww);
+        exit=this.findViewById(R.id.exit_netIcome);
+        AnhXa();
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        Analysis_NetIncome_Adapter adapter=new Analysis_NetIncome_Adapter(arraylist,this,R.layout.analysis_net_income_list_item);
+        listView.setAdapter(adapter);
+
+    }
+    void AnhXa(){
+
+        arraylist=new ArrayList<>();
+        arraylist.add(new AnalysisNetIcome(2019,10000000,200000));
+        arraylist.add(new AnalysisNetIcome(2020,10000000,200000));
+        arraylist.add(new AnalysisNetIcome(2021,10000000,200000));
+        arraylist.add(new AnalysisNetIcome(2022,10000000,200000));
+        arraylist.add(new AnalysisNetIcome(2023,10000000,200000));
+        arraylist.add(new AnalysisNetIcome(2024,10000000,200000));
+
+
     }
 }
--- app/src/main/java/com/example/qlct/AnalysisNetIncomeFragment.java
+++ app/src/main/java/com/example/qlct/AnalysisNetIncomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.qlct;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,7 @@ public class AnalysisNetIncomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    TextView see_detail;
 
     public AnalysisNetIncomeFragment() {
         // Required empty public constructor
@@ -59,6 +62,15 @@ public class AnalysisNetIncomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analysis_net_income, container, false);
+        View view=inflater.inflate(R.layout.fragment_analysis_net_income, container, false);
+        see_detail=view.findViewById(R.id.see_detail_net_income);
+        see_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), AnalysisDetailNetIncome.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
