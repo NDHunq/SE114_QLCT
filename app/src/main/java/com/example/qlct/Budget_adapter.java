7e95e8cb3947222db50083b6412ba40d81e9fe6d
NDHunq <22520514@gm.uit.edu.vn>
2024-03-21 10:38:24 +0700

    Done UI of Budget tab

---
+++ app/src/main/java/com/example/qlct/Budget_adapter.java
@@ -0,0 +1,61 @@
+package com.example.qlct;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class Budget_adapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private List<Budget> listBudget;
+
+    public Budget_adapter(Context context, int layout, List<Budget> listBudget) {
+        this.context = context;
+        this.layout = layout;
+        this.listBudget = listBudget;
+    }
+
+    @Override
+    public int getCount() {
+        return listBudget.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView=inflater.inflate(layout,null);
+        //ánh xạ view
+        ImageView image=convertView.findViewById(R.id.image);
+        TextView category=convertView.findViewById(R.id.category);
+        TextView vi=convertView.findViewById(R.id.vi);
+        TextView fromday=convertView.findViewById(R.id.fromday);
+        TextView today=convertView.findViewById(R.id.today);
+        TextView maxmoney=convertView.findViewById(R.id.maxmoney);
+        //gán giá trị
+        Budget budget=listBudget.get(position);
+        image.setImageResource(budget.getImage());
+        category.setText(budget.getCategory());
+        vi.setText(budget.getVi());
+        fromday.setText(budget.getFromDate());
+        today.setText(budget.getToDate());
+        maxmoney.setText( String.valueOf(budget.getMax_money()));
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/qlct/Budget_fragment.java
+++ app/src/main/java/com/example/qlct/Budget_fragment.java
@@ -1,21 +1,30 @@
 package com.example.qlct;
 
+import static com.example.qlct.R.drawable.background_bo_phai;
+import static com.example.qlct.R.drawable.background_bo_trai;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.TintInfo;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 public class Budget_fragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
-    private TextView create_bud_butt;
+    private TextView create_bud_butt, running, finish;
 
     public Budget_fragment() {
         // Required empty public constructor
@@ -42,13 +51,54 @@ public class Budget_fragment extends Fragment {
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_budget_fragment, container, false);
         create_bud_butt=view.findViewById(R.id.create_bud_butt);
+        running=view.findViewById(R.id.running);
+        finish=view.findViewById(R.id.finish);
         create_bud_butt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getActivity().startActivity(new Intent(getActivity(),AddBudget.class));
             }
-
         });
+        FragmentManager fragmentManager=getChildFragmentManager();
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        Fragment child=new BudgetRunningFragment();
+        transaction.replace(R.id.budget_container,child);
+        transaction.commit();
+        running.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                running.setBackgroundTintList(null);
+                running.setBackgroundResource(background_bo_trai);
+                finish.setBackgroundResource(background_bo_phai);
+                finish.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ACACAC")));
+                running.setTextColor(Color.parseColor("#1EABED"));
+                finish.setTextColor(Color.BLACK);
+
+                FragmentManager fragmentManager2=getChildFragmentManager();
+                FragmentTransaction transaction2=fragmentManager2.beginTransaction();
+                Fragment child1=new BudgetRunningFragment();
+                transaction2.replace(R.id.budget_container,child1);
+                transaction2.commit();
+            }
+        });
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                running.setBackgroundResource(background_bo_trai);
+                finish.setBackgroundTintList(null);
+                finish.setBackgroundResource(background_bo_phai);
+                running.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#ACACAC")));
+                finish.setTextColor(Color.parseColor("#1EABED"));
+                running.setTextColor(Color.BLACK);
+
+                FragmentManager fragmentManager3=getChildFragmentManager();
+                FragmentTransaction transaction3=fragmentManager3.beginTransaction();
+                Fragment child2=new BudgetFinishFragment();
+                transaction3.replace(R.id.budget_container,child2);
+                transaction3.commit();
+            }
+        });
+
         return view;
     }
 }
