eae5ebaf866c200866949fbbd1217a931038b656
redRUM <22521225@gm.uit.edu.vn>
2024-04-12 13:45:59 +0700

    pull

--- app/src/main/java/com/example/qlct/MainActivity.java
+++ app/src/main/java/com/example/qlct/MainActivity.java
@@ -1,23 +1,11 @@
 package com.example.qlct;
 
 
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
-import android.graphics.Color;
-
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
 import android.widget.LinearLayout;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +13,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 import com.example.qlct.databinding.ActivityMainBinding;
 
 
